4c048b0b465f48ced53034b918b270f5f63c959c
Lin2808 <linjaviers@hotmail.com>
2022-09-20 21:17:27 -0500

    Realiza reporte de todos los pacientes de un carer

--- src/app/views/dashboard/dashboard.component.ts
+++ src/app/views/dashboard/dashboard.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { PatientListI } from 'src/app/models/patientlist.interface';
+import { ApiService } from 'src/app/services/api/api.service';
+import { AllpatientsService } from 'src/app/services/report/allpatients.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -8,9 +11,36 @@ import { Router } from '@angular/router';
 })
 export class DashboardComponent implements OnInit {
 
-  constructor(private router : Router) { }
+  patients : PatientListI[] = [];
+  carerId = localStorage.getItem('carerId');
+
+  constructor(private router : Router, private allpatientsService:AllpatientsService, private apiService:ApiService) { }
 
   ngOnInit(): void {
+  }
+
+  printReport()
+  {
+    const header = ["Id", "Name", "Registration date", "Gender", "Birthday"];
+    /*const body = [["1", "Lin", "2022-09-15", "Male","1995-08-28"],
+                  ["1", "Melina", "2022-09-15", "Male","1995-08-28"]];*/
+
+                  this.apiService.getPatientIdCarer(Number(this.carerId)).subscribe(data =>{
+                    const body = Object(data).map(
+                      (obj:any)=>{
+                        const datos = [
+                          obj.id,
+                          obj.name,
+                          obj.registrationDate,
+                          obj.gender,
+                          obj.birthDate
+                        ]
+                        return datos;
+                      }
+                    )
+                    console.log(body);
+                    this.allpatientsService.print(header, body, "Patient list", true);
+                  })
   }
 
 }
--- src/app/views/patients/menupatient/menupatient.component.ts
+++ src/app/views/patients/menupatient/menupatient.component.ts
@@ -12,4 +12,5 @@ export class MenupatientComponent implements OnInit {
   ngOnInit(): void {
   }
 
+
 }
