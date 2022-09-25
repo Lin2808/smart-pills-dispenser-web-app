373e782de8fa7fc7fff5908d939fb5996eba5541
Lin2808 <linjaviers@hotmail.com>
2022-09-24 21:25:16 -0500

    Reportes listos

--- src/app/views/reports/patients/patients.component.ts
+++ src/app/views/reports/patients/patients.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuxDosageI } from 'src/app/models/auxdosage.interface';
+import { DosageI } from 'src/app/models/dosage.interface';
+import { PatientListI } from 'src/app/models/patientlist.interface';
+import { ApiService } from 'src/app/services/api/api.service';
+import { AllpatientsService } from 'src/app/services/report/allpatients.service';
 
 @Component({
   selector: 'app-patients',
@@ -7,9 +13,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PatientsComponent implements OnInit {
 
-  constructor() { }
+  patients : PatientListI[] = [];
+  carerId = localStorage.getItem('carerId');
+  dosageI : DosageI[] = [];
+  containerInformation : AuxDosageI[] = [];
+
+  constructor(private apiService:ApiService, private allpatientsService:AllpatientsService, private router : Router) { }
 
   ngOnInit(): void {
+    this.apiService.getPatientIdCarer(Number(this.carerId)).subscribe((data:any)=>{
+      this.patients = data;
+      console.log(data);
+    })
+  }
+  printYourPatientsReport()
+  {
+    const header = ["Id", "Name", "Gender", "Birthday", "Registration date"];
+    this.apiService.getPatientIdCarer(Number(this.carerId)).subscribe(data =>{
+      const body = Object(data).map(
+        (obj:any)=>{
+          const datos = [
+            obj.id,
+            obj.name,
+            obj.gender,
+            obj.birthDate,
+            obj.registrationDate,
+          ]
+          return datos;
+        })
+        console.log(body);
+        this.allpatientsService.print(header, body, "Patient list", true);
+      }
+    )
   }
 
 }
--- src/app/views/reports/patientsnodosage/patientsnodosage.component.ts
+++ src/app/views/reports/patientsnodosage/patientsnodosage.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuxDosageI } from 'src/app/models/auxdosage.interface';
+import { DosageI } from 'src/app/models/dosage.interface';
+import { PatientI } from 'src/app/models/patient.interface';
+import { ApiService } from 'src/app/services/api/api.service';
+import { AllpatientsService } from 'src/app/services/report/allpatients.service';
 
 @Component({
   selector: 'app-patientsnodosage',
@@ -7,9 +13,92 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PatientsnodosageComponent implements OnInit {
 
-  constructor() { }
+  constructor(private apiService:ApiService, private allpatientsService:AllpatientsService, private router : Router) { }
+
+  dosageI : DosageI[] = [];
+  patients : PatientI[] = [];
+  carerId = localStorage.getItem('carerId');
+  containerInformation : AuxDosageI[] = [];
+
 
   ngOnInit(): void {
+    this.containerInformation = [];
+    const date = new Date();
+    this.apiService.getPatientIdCarer(Number(this.carerId)).subscribe((data:any)=>{
+      this.apiService.getAllDosages().subscribe((dosages:any)=>{
+        for(let dosageList of dosages)
+        {
+          for(let patientList of data)
+          {
+            if(dosageList.medicalTreatment.patient.id == patientList.id)
+            {
+              let dosageDate = new Date(dosageList.dateTake);
+              dosageDate.setMinutes(dosageDate.getMinutes() + 5);
+              dosageDate = new Date(dosageDate);
+              if(dosageDate <= date && dosageList.state == true)
+              {
+                console.log(dosageList.medicalTreatment.patient.name + "   " + dosageList.medicalTreatment.description + "   " + dosageList.prescription + "    " + dosageList.dateTake);
+                this.containerInformation.push({
+                  name : dosageList.medicalTreatment.patient.name,
+                  description : dosageList.medicalTreatment.description,
+                  prescription : dosageList.prescription,
+                  dateTake : dosageList.dateTake
+                });
+              }
+
+            }
+          }
+
+        }
+      })
+    })
+  }
+  printYourPatientsNoDosage()
+  {
+    this.containerInformation = [];
+    const date = new Date();
+    const header = ["Patient", "Medical treatment", "Dosage", "Hour"];
+    this.apiService.getPatientIdCarer(Number(this.carerId)).subscribe((data:any)=>{
+      this.apiService.getAllDosages().subscribe((dosages:any)=>{
+        for(let dosageList of dosages)
+        {
+          for(let patientList of data)
+          {
+            if(dosageList.medicalTreatment.patient.id == patientList.id)
+            {
+              let dosageDate = new Date(dosageList.dateTake);
+              dosageDate.setMinutes(dosageDate.getMinutes() + 5);
+              dosageDate = new Date(dosageDate);
+              if(dosageDate <= date && dosageList.state == true)
+              {
+                //console.log(dosageList.medicalTreatment.patient.name + "   " + dosageList.medicalTreatment.description + "   " + dosageList.prescription + "    " + dosageList.dateTake);
+
+                this.containerInformation.push({
+                  name : dosageList.medicalTreatment.patient.name,
+                  description : dosageList.medicalTreatment.description,
+                  prescription : dosageList.prescription,
+                  dateTake : dosageList.dateTake
+                });
+              }
+            }
+          }
+
+        }
+        const body = Object(this.containerInformation).map(
+          (obj:any)=>{
+            const datos = [
+              obj.name,
+              obj.description,
+              obj.prescription,
+              obj.dateTake
+            ]
+            return datos;
+          })
+          console.log("Body: " + body);
+          this.allpatientsService.print(header, body, "Patient list", true);
+      })
+    })
+
   }
 
 }
